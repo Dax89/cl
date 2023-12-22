d058a863636e1391792b46fe34a3ec1e86178fc5
Dax <contact@antoniodavide.dev>
2023-12-22 17:39:42 +0100

    fix(cmake): catch2 dependency conditions

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,8 +1,8 @@
 include(cmake/CPM.cmake)
 
 function(setup_dependencies)
-    if(BUILD_TESTING)
-        CPMAddPackage("gh:catchorg/Catch2@3.4.0")
+    if(PROJECT_IS_TOP_LEVEL AND BUILD_TESTING)
+        CPMAddPackage("gh:catchorg/Catch2@3.5.0")
     endif()
 endfunction()
 
